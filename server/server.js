8a4a410ee68406491d3aba892e3c3a02ceb80172
inspiredjw <inspired.jw@gmail.com>
2015-02-20 14:26:35 -0600

    google compute engine

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ net.createServer(function(socket) {
     clients.splice(clients.indexOf(socket), 1);
     broadcast(socket.name + ' Left the chat\n');
   });
-}).listen(process.env.PORT || 3000);
+}).listen(3000);
 
 
 // List of clients
