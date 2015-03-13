82ed4ccb24dc214184ee28b6fcfe22f9af0ba9d6
inspiredjw <inspired.jw@gmail.com>
2015-03-13 17:16:11 -0500

    rewrite tcp server part

--- server/server.js
+++ server/server.js
@@ -1,68 +1,39 @@
 var net = require('net');
-var dgram = require('dgram');
-var udpServer = dgram.createSocket('udp4');
 
-var port = 3000;
+var tcpServer = net.createServer(),
+    clientList = [];
 
-var onMessage = function(data) {
-  var buffer = Date.now() + data;
-  socket.write(buffer);
-  broadcast(buffer, socket);
-};
+tcpServer.on('connection', function(client) {
+  client.name = client.remoteAddress + ':' + client.remotePort
+  client.write('Hi ' + client.name + '!\n');
 
-// TCP server
-net.createServer(function(socket) {
-  // A client connected to the server
-  socket.name = refineIP(socket.remoteAddress) + ':' + socket.remotePort;
-  clients.push(socket);
+  clientList.push(client);
 
-  socket.write('welcome ' + socket.name + '\n');
-  broadcast(socket.name + ' joined the chat\n', socket);
-
-  // When the server receives a message from a client
-  socket.on('data', function(data) {
+  client.on('data', function(data) {
     var buffer = Date.now() + data;
-    socket.write(buffer);
-    broadcast(buffer, socket);
+    broadcast(buffer, client);
   });
 
-  // When a client disconnect
-  socket.on('end', function() {
-    clients.splice(clients.indexOf(socket), 1);
-    broadcast(socket.name + ' Left the chat\n');
-  });
-}).listen(port);
-
-
-// List of clients
-var clients = [];
-
-// Strip ::: from IP String
-var refineIP = function(rawIP) {
-  var lastColonIndex = rawIP.lastIndexOf(':');
-  return rawIP.substring(lastColonIndex + 1);
-};
-
-// Broadcast
-var broadcast = function(message, sender) {
-  clients.forEach(function(client) {
-    client.write(message);
-  });
-  process.stdout.write(message + '\n');
-};
-
-
-// UDP server
-udpServer.on('message', function(data) {
-  var buffer = Date.now() + data;
-
-
-  clients.forEach(function(client) {
-    udpServer.send(buffer, 0, buffer.length, client.remotePort, client.remoteAddress, function(err, bytes) {
-      if (err) console.error(err.message);
-    });
+  client.on('end', function() {
+    clientList.splice(clientList.indexOf(client), 1);
   });
 });
 
-udpServer.bind(port);
+function broadcast(message, client) {
+  var cleanup = [];
+  for (var i = 0;i < clientList.length; i += 1) {
+    if (client !== clientList[i]) {
+      if (clientList[i].writable) {
+        clientList[i].write(client.name + " says " + message);
+      } else {
+        cleanup.push(clientList[i]);
+        clientList[i].destroy();
+      }
+    }
+  }
+  for (i = 0;i < cleanup.length; i += 1) {
+    clientList.splice(clientList.indexOf(cleanup[i]), 1);
+  }
+}
 
+tcpServer.listen(3000);
