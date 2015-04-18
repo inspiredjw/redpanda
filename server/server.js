bf0c4e531578de45e931fdbd981a13df16ac188e
inspiredjw <inspired.jw@gmail.com>
2015-04-17 19:00:39 -0500

    udp server added

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 var net = require('net');
+var dgram = require('dgram');
 
 var tcpServer = net.createServer(),
+    udpServer = dgram.createSocket('udp4'),
     clientList = [];
 
 tcpServer.on('connection', function(client) {
-  client.name = client.remoteAddress + ':' + client.remotePort
-  client.write('Hi ' + client.name + '!\n');
+  client.name = client.remoteAddress + ':' + client.remotePort;
+  // client.write('Hi ' + client.name + '!\n');
 
   clientList.push(client);
 
@@ -40,3 +42,16 @@ function broadcast(message, client) {
 }
 
 tcpServer.listen(3000);
+
+
+udpServer.on('message', function(message, remote) {
+  var buffer = Date.now() + data;
+
+  for (var i = 0; i < clientList.length; i++) {
+    udpServer.send(buffer, 0, buffer.length, 3001, clientList[i].name.split()[0]);
+  }
+});
+
+udpServer.bind(3001);
+
+
